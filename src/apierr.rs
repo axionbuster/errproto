eec2ac6109a5a87974c4f13eb7e42ffdc3378002
YuJin Kim <axionbuster@gmail.com>
2023-05-16 14:20:39 -0700

    Initial commit

---
+++ src/apierr.rs
@@ -0,0 +1,98 @@
+//! Api Error Handling
+
+use std::fmt::{Debug, Display};
+
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+
+/// API Result
+pub type Result<R = Response, E = Response> = std::result::Result<R, E>;
+
+/// Return a closure that takes what would be an error type
+/// and then discards it, while giving a default error message to the user.
+///
+/// (A plain text message with the canonical reason, if exists.)
+///
+/// This default responder is used: [`default_response`].
+pub fn stop<C, E, Z>(code: C) -> impl FnOnce(E) -> Response
+where
+    C: TryInto<StatusCode, Error = Z>,
+    Z: Debug,
+{
+    catch(code, |_, _| None::<Response>, default_response)
+}
+
+/// A variety of [`stop`] that, rather than hiding the error from
+/// the user, shows it using a call to [`transparent`].
+pub fn transparent_stop<C, E, Z>(code: C) -> impl FnOnce(E) -> Response
+where
+    C: TryInto<StatusCode, Error = Z>,
+    E: Display,
+    Z: Debug,
+{
+    catch(code, transparent, default_response)
+}
+
+/// Return a closure that takes what would be an error type
+/// and then consumes it, producing an optional custom response.
+/// If the response is generated, it is served to the user.
+/// If the response is not generated, a default error message is given to the user.
+///
+/// NOTE: when the custom response is generated, it's the responsibility of the
+/// caller to set the status code. The `code` provided is only the default
+/// status code to use when the custom response is NOT generated.
+///
+/// HINT: For the default response generator (`default`), you can use
+/// [`default_response`] without any problem.
+pub fn catch<C, E, F, R, Z, D>(code: C, handle: F, default: D) -> impl FnOnce(E) -> Response
+where
+    C: TryInto<StatusCode, Error = Z>,
+    D: Fn(StatusCode) -> Response,
+    F: FnOnce(StatusCode, E) -> Option<R>,
+    R: IntoResponse,
+    Z: Debug,
+{
+    move |e| {
+        let code = cvcode(code);
+        if let Some(r) = handle(code, e) {
+            r.into_response()
+        } else {
+            default(code)
+        }
+    }
+}
+
+/// Create the default error response for a given status code.
+///
+/// HINT: Use as the third argument for the [`catch`] function.
+pub fn default_response(code: StatusCode) -> Response
+where
+{
+    let code = cvcode(code);
+    let body = format!("{}", code); // It gives the numeric code + reason.
+    (code, body).into_response()
+}
+
+/// Create a response by calling Display implementation on an
+/// error type and returning a plain text message.
+///
+/// HINT: Use as the second argument for the [`catch`] function.
+pub fn transparent<D>(code: StatusCode, error: D) -> Option<Response>
+where
+    D: Display,
+{
+    let body = format!("{}", error);
+    Some((code, body).into_response())
+}
+
+/// Convert what could be a status code into a [`StatusCode`], or
+/// panic if the conversion fails.
+fn cvcode<C, Z>(code: C) -> StatusCode
+where
+    C: TryInto<StatusCode, Error = Z>,
+    Z: Debug,
+{
+    code.try_into().expect("invalid status code")
+}
---
+++ src/main.rs
@@ -0,0 +1,108 @@
+mod apierr;
+
+use std::num::ParseIntError;
+
+use axum::{
+    extract::Path,
+    http::{HeaderName, HeaderValue, StatusCode},
+    response::IntoResponse,
+    routing::get,
+    Json, Router, Server,
+};
+use serde::Serialize;
+
+use crate::apierr::*;
+
+fn bad() -> Result<&'static str, &'static str> {
+    Err("bad")
+}
+
+fn good() -> Result<&'static str, &'static str> {
+    Ok("good")
+}
+
+async fn always_500() -> Result<&'static str> {
+    // The user sees: "500 Internal Server Error"
+    // The user does NOT see: "bad"
+    // So, by default, we hide the error from the user.
+    bad().map_err(stop(500))
+}
+
+async fn always_200() -> Result<&'static str> {
+    // The user sees "good" and the status code is 200.
+    good().map_err(stop(500))
+}
+
+async fn error_with_custom_feedback(number: Option<Path<String>>) -> Result<impl IntoResponse> {
+    // I produce a bunch of impl IntoResponse's.
+    let validate_length = |number: String| match number.len() {
+        0 => Err("You must provide a number."),
+        n if n > 5 => Err("The number is too long. Try again with fewer digits."),
+        _ => Ok(number),
+    };
+    let validate_range = |number: i32| match number {
+        ..=68 => Err(format!("The number {number} is too low. Try higher :)")),
+        69 => Ok(format!(
+            "Nice! You guessed the right number, which is {number}!!!"
+        )),
+        70.. => Err(format!("The number {number} is too high. Try lower :)")),
+    };
+    let not_a_number = |c: StatusCode, err: ParseIntError| {
+        // Let's try JSON and some header manipulation just for fun.
+        #[derive(Serialize)]
+        struct X {
+            err: String,
+        }
+        let msg = Json(X {
+            err: err.to_string(),
+        });
+        let set_cookie = "Set-Cookie".parse::<HeaderName>().unwrap();
+        let cookie_value = "foo=bar; Max-Age=10; SameSite=Lax".parse::<HeaderValue>().unwrap();
+        Some((c, [(set_cookie, cookie_value)], msg))
+    };
+
+    let number = match number {
+        Some(Path(number)) => number,
+        None => "".to_string(),
+    };
+
+    // Error handling can be customized using the `catch` function.
+    // `catch` is a generalization of `stop`.
+    //
+    // The second argument (handle) produces an optional response
+    // by combining the status code and the error.
+    //
+    // The default `transparent` function produces a response
+    // by calling Display on the error, and then wrapping it in `Some(_)`.
+    //
+    // The third argument (default) is a function that produces a response
+    // if the second argument (handle) returns `None`.
+    // Here, it's useless, but in other times, it can be useful.
+    let number = validate_length(number).map_err(catch(400, transparent, default_response))?;
+
+    // Now, convert this to an integer!
+    //
+    // Here I use something different than `transparent` to show the error.
+    let number = number
+        .parse::<i32>()
+        .map_err(catch(400, not_a_number, default_response))?;
+
+    // Ok, now, let's check the ranges.
+    // (NOTE: transparent_stop is an alias for catch(_, transparent, default_response)).
+    validate_range(number).map_err(transparent_stop(400))
+}
+
+fn router() -> Router {
+    Router::new()
+        .route("/", get(always_200))
+        .route("/500", get(always_500))
+        .route("/custom", get(error_with_custom_feedback))
+        .route("/custom/:number", get(error_with_custom_feedback))
+}
+
+#[tokio::main]
+async fn main() {
+    let router = router();
+    let app = Server::bind(&"0.0.0.0:3000".parse().unwrap()).serve(router.into_make_service());
+    app.await.unwrap();
+}
